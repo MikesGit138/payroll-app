038eea9efd1817466ced64809cad8f0bec0d6280
MikesGit138 <michael138leighton@gmail.com>
2022-06-25 23:47:28 -0500

    more suitable sql query

--- routes/employees.js
+++ routes/employees.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const DB = require('../lib/db')
 
 router.get('/employees', (req, res) => {
-    let sql = "SELECT * FROM employees"
+    let sql = `select employees.id, employees.frst_nm, employees.lst_nm, employees.dept_id, departments.dept_type
+    from employees join departments on employees.dept_id = departments.id`
     let query = DB.query(sql, (err, rows) =>{
 	    res.render('employees', { title: 'Home', employees: rows })
 

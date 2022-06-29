519b368b676d7ed7102566ba312ca82469981367
MikesGit138 <michael138leighton@gmail.com>
2022-06-29 10:00:27 -0500

    styling changes

--- routes/employees.js
+++ routes/employees.js
@@ -5,15 +5,27 @@ const DB = require('../lib/db')
 router.get('/employees', (req, res) => {
     let sql = `select employees.id, employees.frst_nm, employees.lst_nm, employees.dept_id, departments.dept_type
     from employees join departments on employees.dept_id = departments.id`
-    let query = DB.query(sql, (err, rows) =>{
-	    res.render('employees', { title: 'Home', employees: rows })
+    let query = DB.query(sql, (err, result) =>{
+	    res.render('employees', { title: 'Home', employees: result })
 
     })
 })
 
-router.get('/employees/edit', (req, res) =>{
-    res.render('edit', {title: 'Edit Page'})
-})
+// router.get('/employees/edit/:empId', (req, res) =>{
+//     let empId = req.params.id
+//     let sql = `Select * from employees where id = ${empId}`
+//     let query = DB.query(sql , (err, result) =>{
+//         if (err) throw err      
+//         res.render('edit', {title: 'Edit Page' , employees: result })
+//     })
+// })
+
+// router.post('/update', (req, res)=> {
+//     const empId = req.body.id
+//     const fname = req.body.fname
+//     const lname = req.body.lname
+//     let sql = `Update employees set frst_nm`
+// })
 
 
 
